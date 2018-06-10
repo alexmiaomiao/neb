b22f18608c4b5cbc55465e6202d8428412edb6f2
lzt <2564272712@qq.com>
2018-06-10 23:36:04 +0800

    school

--- main.go
+++ main.go
@@ -8,22 +8,23 @@ import (
 
         "github.com/nebulasio/go-nebulas/storage"
         "github.com/nebulasio/go-nebulas/core"
+        "github.com/nebulasio/go-nebulas/util"
         "github.com/nebulasio/go-nebulas/util/byteutils"
         "github.com/gogo/protobuf/proto"
         "github.com/nebulasio/go-nebulas/core/pb"
-//        "github.com/nebulasio/go-nebulas/rpc"
-//	"github.com/nebulasio/go-nebulas/rpc/pb"
 )
 
 const (
         //mainnet数据库路径
-        dbpath = "/home/lzt/Documents/mygo/src/github.com/alexmiaomiao/neb/data.db"
-	maxRoutine = 5000
-	rpcaddr = "13.251.33.39:8684"
+        dbpath = "/home/lzt/Documents/mygo/src/github.com/nebulasio/go-nebulas/mainnet/data.db"
+	//并发最大协程数
+	maxRoutine = 100000
+	//最早区块的时间戳
+	earliestTS = 1522377345
 )
 
 //获取整个区块链上所有交易单数量
-func getTotalTXs(db *storage.RocksStorage) (uint64, error) {
+func GetTotalTXs(db *storage.RocksStorage) (uint64, error) {
         var currentH uint64
         txChan := make(chan int)
 	sema := make(chan struct{}, maxRoutine)
@@ -73,8 +74,6 @@ func getTotalTXs(db *storage.RocksStorage) (uint64, error) {
         return  <- res, nil
 }
 
-//func rpcGetBlockByHash(hash []byte) (*rpcpb.)
-
 //通过hash获取某个区块
 func getBlockByHash(hash []byte, db *storage.RocksStorage) (*core.Block, error) {
         value, err := db.Get(hash)
@@ -86,10 +85,7 @@ func getBlockByHash(hash []byte, db *storage.RocksStorage) (*core.Block, error) 
 	if err = proto.Unmarshal(value, pbBlock); err != nil {
 		return nil, err
 	}
-	fmt.Println(pbBlock.GetHeader())
-	//fmt.Println(pbBlock)
 	if err = block.FromProto(pbBlock); err != nil {
-		fmt.Println(block)
 		return nil, err
 	}
 	return block, nil
@@ -106,23 +102,19 @@ func getBlockByHeight(h uint64, db *storage.RocksStorage) (*core.Block, error) {
 
 //得到最后一个区块
 func getTailBlock(db *storage.RocksStorage) (*core.Block, error) {
-        if hash, err := db.Get([]byte(core.Tail)); err != nil {
+        hash, err := db.Get([]byte(core.Tail))
+	if err != nil {
                 return nil, err
-        } else {
-                if b, err := getBlockByHash(hash, db); err != nil {
-                        return nil, err
-                } else {
-                        return b, nil
-		}
         }
+	return getBlockByHash(hash, db)
 }
 
-func heightInPeriod(ts, te time.Time, hs, he uint64, db *storage.RocksStorage) (uint64, error) {
+//二分法快速定位一个时间段的某个区块高度
+func heightInPeriod(ts, te time.Time, hs, he uint64, db *storage.RocksStorage) (uint64, core.Transactions, error) {
 	hm := (hs + he) / 2
         block, err := getBlockByHeight(hm, db)
 	if err != nil {
-		fmt.Println(hm, err)
-                return 0, err
+                return 0, nil, err
         }
 	bt := block.Timestamp()
 	if bt < ts.Unix() {
@@ -131,10 +123,11 @@ func heightInPeriod(ts, te time.Time, hs, he uint64, db *storage.RocksStorage) (
 	if bt > te.Unix() {
 		return heightInPeriod(ts, te, hs, hm-1, db)
 	}
-	return hm, nil
+	return hm, block.Transactions(), nil
 }
 
-func getTXsByDay(y,m,d int, db *storage.RocksStorage) (*core.Transactions, error) {
+//获取某天的全部交易单
+func getTXsByDay(y,m,d int, db *storage.RocksStorage) (core.Transactions, error) {
 	ts := time.Date(y, time.Month(m), d, 0, 0, 0, 0, time.Local)
 	te := ts.Add(24*time.Hour)
 	
@@ -142,17 +135,117 @@ func getTXsByDay(y,m,d int, db *storage.RocksStorage) (*core.Transactions, error
 	if err != nil {
                 return nil, err
         }
-	
-	hh, err := heightInPeriod(ts, te, 2, block.Height(), db)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(hh)
 
-	return nil, nil
+	if te.Unix() < earliestTS || ts.Unix() > block.Timestamp() {
+		return nil, fmt.Errorf("The date is not in interval")
+	}
+	
+	hm, TX, err := heightInPeriod(ts, te, 2, block.Height(), db)
+	if err != nil {
+		return nil, err
+	}
+
+	TXChan := make(chan core.Transactions)
+        var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func(hh uint64) {
+		for i:=hh; ; i++ {
+			block, err := getBlockByHeight(i, db)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if block.Timestamp() > te.Unix() {
+				break
+			}
+			TXChan <- block.Transactions()
+		}
+		wg.Done()
+	}(hm+1)
+
+	go func(hh uint64) {
+		for i:=hh; ; i-- {
+			block, err := getBlockByHeight(i, db)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if block.Timestamp() < ts.Unix() {
+				break
+			}
+			TXChan <- block.Transactions()
+		}
+		wg.Done()
+	}(hm-1)
+
+	go func() {
+                wg.Wait()
+                close(TXChan)
+        }()
+
+	for tx := range TXChan {
+		TX = append(TX, tx...)
+	}
+
+	return TX, nil
+}
+
+//整理交易单
+func arrangeTXs(TXs core.Transactions) (binary, call, deploy core.Transactions) {
+	for _, tx := range TXs {
+		switch tx.Type() {
+		case core.TxPayloadBinaryType:
+			binary = append(binary, tx)
+		case core.TxPayloadCallType:
+			call = append(call, tx)
+		case core.TxPayloadDeployType:
+			deploy = append(deploy, tx)
+		}
+	}
+	return
+}
+
+//统计合约调用次数
+func countContractCall(TXs core.Transactions) (map[string]int) {
+	m := make(map[string]int)
+	for _, tx := range TXs {
+		m[tx.To().String()] += 1
+	}
+	return m
+}
+
+//统计合约入账出账
+func countContractInOut(addr string, TXs core.Transactions) ([2]*util.Uint128, error) {
+	Addr, err := core.AddressParse(addr)
+	inout := [2]*util.Uint128{util.NewUint128(), util.NewUint128()}
+	if err != nil {
+		return inout, err
+	}
+	b, c, _ := arrangeTXs(TXs)
+	for _, tx := range c {
+		if tx.To().Equals(Addr) {
+			inout[0], err = inout[0].Add(tx.Value())
+			if err != nil {
+				return inout, err
+			}
+		}
+	}
+	for _, tx := range b {
+		if tx.From().Equals(Addr) {
+			log.Fatal()
+			inout[1], err = inout[1].Add(tx.Value())
+			if err != nil {
+				return inout, err
+			}
+		}
+	}
+	return inout, nil
 }
 
 func main() {
+	core.SetCompatibilityOptions(core.MainNetID)
+	
         db, err := storage.NewRocksStorage(dbpath)
 
         if err != nil {
@@ -161,8 +254,7 @@ func main() {
         }
         defer db.Close()
 
-	//224013
-        if block, err := getBlockByHeight(uint64(224013), db); err != nil {
+        if block, err := getBlockByHeight(uint64(410511), db); err != nil {
                 log.Fatal(err)
         } else {
                 fmt.Println(block)
@@ -171,11 +263,27 @@ func main() {
                 }
         }
 
-        // if num, err := getTotalTXs(db); err != nil {
+        // if num, err := GetTotalTXs(db); err != nil {
         //         log.Fatal(err)
 	// } else {
         //         fmt.Println(num)
         // }
 
-	//getTXsByDay(2018, 5, 5, db)
+	// if txs, err := getTXsByDay(2018, 6, 2, db); err != nil {
+	// 	log.Fatal(err)
+	// } else {
+	// 	b, c, d := arrangeTXs(txs)
+	// 	fmt.Println(b)
+	// 	fmt.Println(c)
+	// 	fmt.Println(d)
+	// 	m := countContractCall(c)
+	// 	for k, v := range m {
+	// 		fmt.Printf("%s\t%d\n", k, v)
+	// 		cio, err := countContractInOut(k, txs)
+	// 		if err != nil {
+	// 			log.Fatal(err)
+	// 		}
+	// 		fmt.Println(cio)
+	// 	}
+	// }
 }
